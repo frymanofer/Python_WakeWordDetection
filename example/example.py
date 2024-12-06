d6175a19a92e12b64f0265906ddcd230a8a0f0e6
Ofer Fryman <ofer@getecho.io>
2024-12-06 16:08:39 +0200

    New version

--- example/example.py
+++ example/example.py
@@ -1,18 +1,21 @@
 # example.py
-
+import time
 from keyword_detection import KeywordDetection
+import asyncio
+import threading
 
 def detection_callback(phrase):
     print(f"detection_callback() Detected phrase: {phrase}")
+    for seconds_left in range(5, -1, -1):
+        print(f"Please wait for {seconds_left} seconds before calling '{phrase}' again.")
+        time.sleep(1)
 
-if __name__ == "__main__":
+async def main():
     # The array of models to be used:
-    
     keyword_detection_models = ["models/need_help_now.onnx"]
-#    keyword_detection_models = ["models/lunafit_done_model_28_07112024.onnx"]
     keyword_model = KeywordDetection(keyword_models=keyword_detection_models)
     #license for the library:
-    license_key = "MTczMjkxNzYwMDAwMA==-DDwBWs914KpHbWBBSqi28vhiM4l5CYG+YgS2n9Z3DMI="
+    license_key = "MTczNDIxMzYwMDAwMA==-tNV5HJ3NTRQCs5IpOe0imza+2PgPCJLRdzBJmMoJvok="
     keyword_model.set_keyword_detection_license(license_key)
     for keyword_models_name in keyword_model.keyword_models_names:
         print ("model_name = ", keyword_models_name)
@@ -28,9 +31,19 @@ if __name__ == "__main__":
     # gateway count values:
     # 2 - default
     # number between 1 and 10.
-    keyword_model.set_keyword_detection_threshold_and_gateway_count('high', 2)
+#    keyword_model.set_keyword_detection_threshold_and_gateway_count('medium', 2)
+    keyword_model.set_keyword_detection_threshold_and_gateway_count('medium', 2)
 
     # Use this to loop forever: 
-    keyword_model.start_keyword_detection()
+    # keyword_model.start_keyword_detection()
+    thread = threading.Thread(target=keyword_model.start_keyword_detection)
+    thread.start()
+    print(f"Thread created start_keyword_detection()")
+
+    # await keyword_model.start_keyword_detection()
     # or setup an async call:
     # asyncio.create_task(keyword_model.start_keyword_detection())  # This will run in the background
+    
+if __name__ == "__main__":
+    # Run the asyncio event loop
+    asyncio.run(main())
