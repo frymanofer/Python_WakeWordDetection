d68c18e2fa00ed18df3ad6a146d95b4a2c1a911c
Ofer Fryman <ofer@getecho.io>
2024-12-09 16:15:26 +0200

    New lib version 1.0.18

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ PYTHON_VERSION=3.12
 source venv${PYTHON_VERSION}/bin/activate
 
 # Set the right library of keyword_detection
-export KEYWORD_DETECTION_VERSION="1.0.17"
+export KEYWORD_DETECTION_VERSION="1.0.18"
 
 # Capture the output of the Python script
 command=$(python install.py)
