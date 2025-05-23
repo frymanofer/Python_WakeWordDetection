f82d06adf8546926f9dbc10e467e11da1c354307
Ofer Fryman <ofer@getecho.io>
2025-05-23 17:37:42 +0300

    New wait_time arg in ms between callbacks.

--- example/example.py
+++ example/example.py
@@ -5,10 +5,12 @@ import asyncio
 import threading
 import os
 
-def secondary_detection_callback(params):
+def lower_threshold_callback(params):
     """Secondary detection callback with structured params."""
-    print(f"secondary_detection_callback()",)
-    print(f"secondary_detection_callback() Detected phrase: {params['phrase']} threshold_score: {params['threshold_scores']}")
+    print(f"THIS IS NOT A DETECTION!!!!!",)
+    print(f"THIS IS JUST TO INFORM THAT WE GOT HIGHER THAN USUAL THRESHOLD !!!!!",)
+    print(f"Threshold is : {params['threshold_scores']}")
+    print(f"Recommended to save the activation sound for continuously improving the model Detected phrase: {params['phrase']} threshold_score: {params['threshold_scores']}")
 
 def detection_callback(params):
     """Main detection callback with structured params."""
@@ -30,8 +32,9 @@ async def main():
         {
             "model_path": "models/hey_lookdeep_model_28_06032025_bno22.onnx",
             "callback_function": detection_callback,
-            "threshold": 0.99,
-            "buffer_cnt": 4
+            "threshold": 0.9,
+            "buffer_cnt": 4,
+            "wait_time": 50 # wait in ms
         },
          # Add more models here:
         #,
@@ -41,6 +44,7 @@ async def main():
         #     "threshold": 0.9999,
         #     "buffer_cnt": 3
         # }
+
     ]
     
     keyword_model = KeywordDetection(keyword_models=keyword_detection_models)
@@ -56,7 +60,7 @@ async def main():
     keyword_model.set_keyword_detection_license(license_key)
     for keyword_models_name in keyword_model.keyword_models_names:
         #keyword_model.set_callback(keyword_model_name=keyword_models_name,callback=detection_callback)
-        keyword_model.set_secondary_callback(keyword_model_name=keyword_models_name,callback=secondary_detection_callback, secondary_threshold=0.9)
+        keyword_model.set_secondary_callback(keyword_model_name=keyword_models_name,callback=lower_threshold_callback, secondary_threshold=0.9)
     
     # Use this to loop forever: 
     #thread = threading.Thread(target=keyword_model.start_keyword_detection)
