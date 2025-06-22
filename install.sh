0c1912176eee1244de329f62766b6ee37e5a298b
Ofer Fryman <ofer@getecho.io>
2025-06-22 17:59:24 +0300

    Change install scripts to version 2.0.3

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ PYTHON_VERSION=3.10
 source venv${PYTHON_VERSION}/bin/activate
 
 # Set the right library of keyword_detection
-export KEYWORD_DETECTION_VERSION="2.0.1"
+export KEYWORD_DETECTION_VERSION="2.0.3"
 
 # Capture the output of the Python script
 command=$(python install.py)
